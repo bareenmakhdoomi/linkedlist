cecf0a65fe40dbb88aa7bb0a886e4980c2bebc4c
bareenmakhdoomi <81918142+bareenmakhdoomi@users.noreply.github.com>
2021-09-15 11:07:21 +0530

    Update llinsert.c

--- llinsert.c
+++ llinsert.c
@@ -12,28 +12,29 @@ int main()
 {
 	
      int ch;
-     	printf("1 for create\n 2 for view\n");
+     	printf("1 for create\n 2 for view\n");//asking for users choice
      	printf("enter your choice");
      	scanf("%c",&ch);
      	switch(ch)
 		 {
-     		case '1':create();
+     		case '1':create();//caliing create function
      		    break;
-     		case '2':view();
+     		case '2':view();//caliing view function
      		    break;
 			default:printf("wrong choice!");
 		 }
     return 0;
 }
+//code for create function
 void create()
 {
 	int n,i;
 	struct node *temp;
-	printf("enter number of nodes to be created ");
+	printf("enter number of nodes to be created ");//asking for number of nodes from user
      	scanf("%d",&n);
-     	for(i=1;i<n;i++)
+     	for(i=1;i<n;i++)//storing data in memory
      	{
-     		temp=(struct node*)malloc(sizeof(struct node));
+     		temp=(struct node*)malloc(sizeof(struct node));//malloc allocates sizeof(struct node) bytes, and returns a void pointer to it, which we cast to struct node*
      		printf("enter data");
      	    scanf("%d",&temp->data);
      	    temp->next=NULL;
@@ -50,6 +51,7 @@ void create()
 			 i=i+1;
 		 }
 	}
+//code for view function
 	void view()
 	{
 		struct node *t;
