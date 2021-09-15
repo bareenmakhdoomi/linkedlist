6c22d0a2f058ee6b5dc10cb3e4e8dca437483f4a
bareenmakhdoomi <81918142+bareenmakhdoomi@users.noreply.github.com>
2021-09-15 11:01:15 +0530

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -7,20 +7,19 @@ struct node
 int main()
 {
 	struct node n1,n2,n3,n4,n5;
-	n1.data=10;
-	n2.data=20;
-	n3.data=30;
-	n4.data=40;
-	n5.data=50;
+	n1.data=10;//acessing data to node n1
+	n2.data=20;//acessing data to node n2
+	n3.data=30;//acessing data to node n3
+	n4.data=40;//acessing data to node n4
+	n5.data=50;//acessing data to node n5
 	n1.next=&n2;
 	n2.next=&n3;
 	n3.next=&n4;
 	n4.next=&n5;
 	printf("%d\n",n1.data);
-	printf("%d\n",n1.next->data);
-	printf("%d\n",n1.next->next->data);
-	printf("%d\n",n1.next->next->next->data);
-	printf("%d\n",n1.next->next->next->next->data);
-	printf("%d\n",n1.next->next->next->next->next->data);
+	printf("%d\n",n1.next->data);//acessing node 2 from n1
+	printf("%d\n",n1.next->next->data);//acessing node 3 from n1
+	printf("%d\n",n1.next->next->next->data);//acessing node 4 from n1
+	printf("%d\n",n1.next->next->next->next->data);//acessing node 5 from n1
 	return 0;
 }
