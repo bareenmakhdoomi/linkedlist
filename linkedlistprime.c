8ac0ca29980a414c9509a89e1c9f5af6923082c4
bareenmakhdoomi <81918142+bareenmakhdoomi@users.noreply.github.com>
2021-09-25 18:45:20 +0530

    Add files via upload

---
+++ linkedlistprime.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<malloc.h>
+void create();
+void view();
+struct node
+{
+	char data;
+	struct node *next;
+};
+struct node *head=NULL,*tail=NULL;
+int main()
+{
+     	printf("values of linked list\n");
+     	create();
+     	view();
+    return 0;
+}
+//code for create function
+void create()
+{
+	int n,i,j;
+	struct node *temp;
+	printf("enter number of nodes to be created ");//asking for number of nodes from user
+     	scanf("%d",&n);
+     	for(i=0;i<n;i++)//storing data in memory
+     	{
+     		temp=(struct node*)malloc(sizeof(struct node));//malloc allocates sizeof(struct node) bytes, and returns a void pointer to it, which we cast to struct node*
+     		printf("enter data");
+     	    scanf("%d",&temp->data);
+     	    for(j=2;j<temp->data/2;j++)
+     	    {
+			 if(temp->data%j!=0)
+     	    	{
+     	    temp->next=NULL;
+     	    if(head==NULL)
+     	    {
+			head=temp;
+     	    tail=temp;
+			 }
+			 else
+			 {
+			 	tail->next=temp;
+			 	tail=temp;
+			 }
+			 
+				 }
+     	    
+		 }
+			 }
+     	
+	}
+		void view()
+	{
+		struct node *t;
+		t=head;
+		while(t!=NULL)
+		{
+			printf("%d\t",t->data);
+			t=t->next;
+		}
+	}
